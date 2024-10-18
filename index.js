43e19ee2d693d36e78a3db0f16cdb8b6567715bb
e.mittendorfer <elisabeth.mitten@gmail.com>
2024-10-18 09:10:31 +0200

    add personalice hello

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/bye', (req, res) => {
     res.send('Goodbye World!');
 });
 
+app.get('/hello/:name', (req, res) => {
+    res.send(`Hello ${req.params.name}!`);
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
